1edb27f436286fe220b20706f4644cc46f981807
Aswin Infant Joseph <ashinfant@gmail.com>
2021-10-11 19:03:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-
-app.listen(3000, function() {
+var port = process.env.PORT || 3000
+app.listen(port, function() {
 	console.log('server running on port 3000');
 } )
 
