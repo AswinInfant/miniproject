a9994207f7d3cf5f52bb589c50671ada7df1c2e0
Aswin Infant Joseph <ashinfant@gmail.com>
2021-10-11 19:43:37 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,10 +2,11 @@ import sys
 import requests
 
 api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
-query = sys.argv[1]
+query = 'banana'
 response = requests.get(api_url + query, headers={'X-Api-Key': 'YXC9wHz0W/ebIP7HTJvnSA==6I0nMhcEbdoFzA5R'})
 if response.status_code == requests.codes.ok:
     print(response.text)
+    print(sys.argv[1])
 else:
     print("Error:", response.status_code, response.text)
 
