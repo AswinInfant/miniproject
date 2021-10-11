7be918d01da74c62f21e7d0bfee9b70b25c90303
Aswin Infant Joseph <ashinfant@gmail.com>
2021-10-11 19:51:47 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,7 @@
+
 import sys
-import requests
-
-api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
-query = sys.argv[1]
-response = requests.get(api_url + query, headers={'X-Api-Key': 'YXC9wHz0W/ebIP7HTJvnSA==6I0nMhcEbdoFzA5R'})
-if response.status_code == requests.codes.ok:
-    print(response.text)
-else:
-    print("Error:", response.status_code, response.text)
-
+# Takes first name and last name via command 
+# line arguments and then display them
+print("Output from Python")
+print("First name: " + sys.argv[1])
+print("Last name: " + sys.argv[2])
