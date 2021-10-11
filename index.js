be0a248cf14d5e4c8b1cd47ed9e2643f2219407d
Aswin Infant Joseph <ashinfant@gmail.com>
2021-10-11 19:17:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ app.listen(port, function() {
 	console.log('server running on port 3000');
 } )
 
-app.get('/name', callName);
+app.get('/food', callName);
 function callName(req, res) {
 	var spawn = require("child_process").spawn;
-	var process = spawn('python',["./hello.py", req.query.firstname, req.query.lastname] );
+	var process = spawn('python',["./hello.py", req.query.foodname] );
 	process.stdout.on('data', function(data) {
 		res.send(data.toString());
 	} )
