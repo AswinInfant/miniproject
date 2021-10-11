334a1131813e11c116ad574af2f5a7de005fe719
Aswin Infant Joseph <ashinfant@gmail.com>
2021-10-11 19:17:48 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 import sys
-print("Output from Python")
-print("First name: " + sys.argv[1])
-print("Last name: " + sys.argv[2])
-print("from python script")
+import requests
+
+api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
+query = sys.argv[1]
+response = requests.get(api_url + query, headers={'X-Api-Key': 'YXC9wHz0W/ebIP7HTJvnSA==6I0nMhcEbdoFzA5R'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
+
