124cdd362447b8a5f9582ace89d723266b20a653
Aswin Infant Joseph <ashinfant@gmail.com>
2021-10-12 07:13:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 var express = require('express');
+const request = require('request');
 var app = express();
 var port = process.env.PORT || 3000
 app.listen(port, function() {
 	console.log('server running on port 3000');
 } )
 
-app.get('/name', callName);
-//app.get('/food', callName);
+app.get('/food', callName);
 function callName(req, res) {
 	var spawn = require("child_process").spawn;
-	var process = spawn('python',["./hello.py", req.query.firstname, req.query.lastname] );
-	//var process = spawn('python',["./hello.py", req.query.foodname] );
+	var process = spawn('python',["./hello.py", req.query.foodname] );
 	process.stdout.on('data', function(data) {
 		res.send(data.toString());
 	} )
+}
+
+app.get('/details', callDetails);
+function callDetails(req,res) {
+
+var query = '3lb carrots and a chicken sandwich';
+request.get({
+  url: 'https://api.calorieninjas.com/v1/nutrition?query='+query,
+  headers: {
+    'X-Api-Key': 'YXC9wHz0W/ebIP7HTJvnSA==6I0nMhcEbdoFzA5R'
+  },
+}, function(error, response, body) {
+  if(error) return console.error('Request failed:', error);
+  else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+  else res.send(body);
+});
 }
 
